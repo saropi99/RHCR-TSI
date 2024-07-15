37fa4d1f5037ecc4c32568eff3a8156348988781
Justin Shetty <jas0504@gmail.com>
2024-07-15 18:07:02 +0900

    error handling

--- src/SingleAgentSolver.cpp
+++ src/SingleAgentSolver.cpp
@@ -4,11 +4,18 @@
 double SingleAgentSolver::compute_h_value(const BasicGraph& G, int curr, int goal_id,
                              const vector<pair<int, int> >& goal_location) const
 {
-    double h = G.heuristics.at(goal_location[goal_id].first)[curr];
+    assert(goal_id < goal_location.size());
+    auto goal = goal_location[goal_id].first;
+    if(!G.heuristics.contains(goal))
+    {
+        cout << "ERROR: h value not precomputed for the given goal id" << endl;
+        exit(-1);
+    }
+    double h = G.heuristics.at(goal)[curr];
     goal_id++;
     while (goal_id < (int) goal_location.size())
     {
-        h += G.heuristics.at(goal_location[goal_id].first)[goal_location[goal_id - 1].first];
+        h += G.heuristics.at(goal)[goal_location[goal_id - 1].first];
         goal_id++;
     }
     return h;

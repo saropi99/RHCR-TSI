3e13b6a32edfa6ca9e01aea147e6e9b987f27699
Justin Shetty <jas0504@gmail.com>
2024-07-22 22:27:11 +0900

    offload heuristic calc to separate func

--- src/SingleAgentSolver.cpp
+++ src/SingleAgentSolver.cpp
@@ -1,22 +1,24 @@
 #include "SingleAgentSolver.h"
 
+const std::vector<double>& get_heuristic(BasicGraph& G, int location)
+{
+    if(G.heuristics.find(location) == G.heuristics.end())
+    {
+        // std::cout << "calculating heuristic for " << G.human_readable_loc(location) << "..." << std::flush;
+        G.heuristics[location] = G.compute_heuristics(location);
+        // std::cout << "done" << std::endl;
+    }
+    return G.heuristics[location];
+}
 
 double SingleAgentSolver::compute_h_value(BasicGraph& G, int curr, int goal_id,
                              const vector<pair<int, int> >& goal_location) const
 {
-    assert(goal_id < goal_location.size());
-    auto goal = goal_location[goal_id].first;
-    if(!G.heuristics.count(goal))
-    {
-        std::cout << "calculating heuristic for " << G.human_readable_loc(goal) << "..." << std::flush;
-        G.heuristics[goal] = G.compute_heuristics(goal);
-        std::cout << "done" << std::endl;
-    }
-    double h = G.heuristics.at(goal)[curr];
+    double h = get_heuristic(G, goal_location[goal_id].first)[curr];
     goal_id++;
     while (goal_id < (int) goal_location.size())
     {
-        h += G.heuristics.at(goal)[goal_location[goal_id - 1].first];
+        h += get_heuristic(G, goal_location[goal_id].first)[goal_location[goal_id - 1].first];
         goal_id++;
     }
     return h;
